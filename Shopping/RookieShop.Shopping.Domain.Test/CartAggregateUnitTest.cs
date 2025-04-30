0748fe998ced4c79f69aebb6bc0b0876c0158266
Khoa Huynh Bach <caigicungduoc23@gmail.com>
2025-04-30 19:17:53 +0700

    Add unit test for the domain layer of Shopping module.

---
+++ Shopping/RookieShop.Shopping.Domain.Test/CartAggregateUnitTest.cs
@@ -0,0 +1,224 @@
+using RookieShop.Shopping.Domain.Events;
+
+namespace RookieShop.Shopping.Domain.Test;
+
+public class CartAggregateUnitTest
+{
+    [Fact]
+    public void Test_CartCreation()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        
+        // Act
+        var cart = new Cart(id);
+        
+        // Assert
+        Assert.Equal(id, cart.Id);
+        Assert.Empty(cart.Items);
+    }
+
+    [Fact]
+    public void Should_AddItem_SuccessWithNewItem()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("abc", "ABC", 20, Guid.NewGuid(), 1);
+        cart.ClearDomainEvents();
+        
+        // Act
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        
+        // Assert
+        var item = cart.Items.FirstOrDefault(item => item.Sku == "sku");
+        
+        Assert.NotNull(item);
+        Assert.Equal("name", item.Name);
+        Assert.Equal(10, item.Price);
+        Assert.Equal(3, item.Quantity);
+
+        Assert.Contains(cart.DomainEvents, domainEvent =>
+        {
+            var isItemAddedToCart = domainEvent is ItemAddedToCart;
+
+            if (!isItemAddedToCart)
+            {
+                return false;
+            }
+
+            var itemAddedToCart = (ItemAddedToCart)domainEvent;
+
+            return itemAddedToCart is { Sku: "sku", Quantity: 3 };
+        });
+    }
+    
+    [Fact]
+    public void Should_AddItem_SuccessWithAddedQuantityToExistingItem()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        cart.ClearDomainEvents();
+        
+        // Act
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 2);
+        
+        // Assert
+        var item = cart.Items.FirstOrDefault(item => item.Sku == "sku");
+        
+        Assert.NotNull(item);
+        Assert.Equal("name", item.Name);
+        Assert.Equal(10, item.Price);
+        Assert.Equal(5, item.Quantity);
+
+        Assert.Contains(cart.DomainEvents, domainEvent =>
+        {
+            var isItemAddedToCart = domainEvent is ItemAddedToCart;
+
+            if (!isItemAddedToCart)
+            {
+                return false;
+            }
+
+            var itemAddedToCart = (ItemAddedToCart)domainEvent;
+
+            return itemAddedToCart is { Sku: "sku", Quantity: 2 };
+        });
+    }
+    
+    [Fact]
+    public void Should_AddItem_SuccessWithUnchangedItemInfo()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        cart.ClearDomainEvents();
+        
+        // Act
+        cart.AddItem("sku", "name name", 100, Guid.NewGuid(), 2);
+        
+        // Assert
+        var item = cart.Items.FirstOrDefault(item => item.Sku == "sku");
+        
+        Assert.NotNull(item);
+        Assert.Equal("name", item.Name);
+        Assert.Equal(10, item.Price);
+        Assert.Equal(5, item.Quantity);
+    }
+
+    [Fact]
+    public void Should_AdjustItemQuantity_FailedWithNotFoundItem()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        cart.ClearDomainEvents();
+        
+        // Act
+        var adjustItemQuantityAction = () => cart.AdjustItemQuantity("abc", 4);
+        
+        // Assert
+        Assert.Throws<CartItemNotFoundException>(adjustItemQuantityAction);
+    }
+
+    [Fact]
+    public void Should_AdjustItemQuantity_SuccessWithNothingHappens()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        cart.ClearDomainEvents();
+        
+        // Act
+        cart.AdjustItemQuantity("sku", 3);
+        
+        // Assert
+        Assert.Empty(cart.DomainEvents);
+    }
+
+    [Fact]
+    public void Should_AdjustItemQuantity_Success()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        cart.ClearDomainEvents();
+        
+        // Act
+        cart.AdjustItemQuantity("sku", 5);
+        
+        // Assert
+        var item = cart.Items.FirstOrDefault(item => item.Sku == "sku");
+        
+        Assert.NotNull(item);
+        Assert.Equal(5, item.Quantity);
+
+        Assert.Contains(cart.DomainEvents, domainEvent =>
+        {
+            var isItemQuantityAdjusted = domainEvent is ItemQuantityAdjusted;
+
+            if (!isItemQuantityAdjusted)
+            {
+                return false;
+            }
+
+            var itemQuantityAdjusted = (ItemQuantityAdjusted)domainEvent;
+
+            return itemQuantityAdjusted is { Sku: "sku", OldQuantity: 3, NewQuantity: 5 };
+        });
+    }
+
+    [Fact]
+    public void Should_RemoveItem_FailedWithNotFoundItem()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        cart.ClearDomainEvents();
+        
+        // Act
+        var removeItemAction = () => cart.RemoveItem("abc");
+        
+        // Assert
+        Assert.Throws<CartItemNotFoundException>(removeItemAction);
+    }
+
+    [Fact]
+    public void Should_RemoveItem_Success()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var cart = new Cart(id);
+        cart.AddItem("sku", "name", 10, Guid.NewGuid(), 3);
+        cart.ClearDomainEvents();
+        
+        // Act
+        cart.RemoveItem("sku");
+        
+        // Assert
+        var item = cart.Items.FirstOrDefault(item => item.Sku == "sku");
+        
+        Assert.Null(item);
+
+        Assert.Contains(cart.DomainEvents, domainEvent =>
+        {
+            var isItemRemovedFromCart = domainEvent is ItemRemovedFromCart;
+
+            if (!isItemRemovedFromCart)
+            {
+                return false;
+            }
+
+            var itemRemovedFromCart = (ItemRemovedFromCart)domainEvent;
+
+            return itemRemovedFromCart is { Sku: "sku", Quantity: 3 };
+        });
+    }
+}
---
+++ Shopping/RookieShop.Shopping.Domain.Test/StockItemAggregateUnitTest.cs
@@ -0,0 +1,203 @@
+using RookieShop.Shopping.Domain.Events;
+
+namespace RookieShop.Shopping.Domain.Test;
+
+public class StockItemAggregateUnitTest
+{
+    [Fact]
+    public void Test_StockItemCreation()
+    {
+        // Arrange
+        var sku = "sku";
+        var name = "name";
+        var price = 100;
+        var imageId = Guid.NewGuid();
+        
+        // Act
+        var stockItem = new StockItem(sku, name, price, imageId);
+        
+        // Assert
+        Assert.Equal(sku, stockItem.Sku);
+        Assert.Equal(name, stockItem.Name);
+        Assert.Equal(price, stockItem.Price);
+        Assert.Equal(imageId, stockItem.ImageId);
+        Assert.Equal(0, stockItem.AvailableQuantity);
+        Assert.Equal(0, stockItem.ReservedQuantity);
+    }
+
+    [Fact]
+    public void Test_UpdateInfo()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        var newName = "newName";
+        var newPrice = 200;
+        var newImageId = Guid.NewGuid();
+        
+        // Act
+        stockItem.UpdateInfo(newName, newPrice, newImageId);
+        
+        // Assert
+        Assert.Equal(newName, stockItem.Name);
+        Assert.Equal(newPrice, stockItem.Price);
+        Assert.Equal(newImageId, stockItem.ImageId);
+    }
+
+    [Fact]
+    public void Test_AddUnits()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        stockItem.AddUnits(20);
+        stockItem.ClearDomainEvents();
+        
+        // Act
+        stockItem.AddUnits(10);
+        
+        // Assert
+        Assert.Equal(30, stockItem.AvailableQuantity);
+
+        Assert.Contains(stockItem.DomainEvents, domainEvent =>
+        {
+            var isStockLevelChanged = domainEvent is StockLevelChanged;
+
+            if (!isStockLevelChanged)
+            {
+                return false;
+            }
+            
+            var stockLevelChanged = (StockLevelChanged)domainEvent;
+
+            return stockLevelChanged is { Sku: "sku", ChangedQuantity: 10 };
+        });
+    }
+
+    [Fact]
+    public void Should_Reserve_FailedWithNotEnoughStock()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        stockItem.AddUnits(5);
+        stockItem.Reserve(3);
+        stockItem.ClearDomainEvents();
+        
+        // Act
+        var reserveAction = () => stockItem.Reserve(4);
+        
+        // Assert
+        Assert.Throws<NotEnoughUnitsToReserveException>(reserveAction);
+    }
+
+    [Fact]
+    public void Should_Reserve_Success()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        stockItem.AddUnits(5);
+        stockItem.Reserve(2);
+        stockItem.ClearDomainEvents();
+        
+        // Act
+        stockItem.Reserve(2);
+        
+        // Assert
+        Assert.Equal(1, stockItem.AvailableQuantity);
+        Assert.Equal(4, stockItem.ReservedQuantity);
+        
+        Assert.Contains(stockItem.DomainEvents, domainEvent =>
+        {
+            var isStockLevelChanged = domainEvent is StockLevelChanged;
+
+            if (!isStockLevelChanged)
+            {
+                return false;
+            }
+            
+            var stockLevelChanged = (StockLevelChanged)domainEvent;
+
+            return stockLevelChanged is { Sku: "sku", ChangedQuantity: -2 };
+        });
+    }
+
+    [Fact]
+    public void Should_ConfirmReservation_Failed()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        stockItem.AddUnits(5);
+        stockItem.Reserve(2);
+        stockItem.ClearDomainEvents();
+        
+        // Act
+        var confirmReservationAction = () => stockItem.ConfirmReservation(3);
+        
+        // Assert
+        Assert.Throws<InvalidOperationException>(confirmReservationAction);
+    }
+    
+    [Fact]
+    public void Should_ConfirmReservation_Success()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        stockItem.AddUnits(5);
+        stockItem.Reserve(4);
+        stockItem.ClearDomainEvents();
+        
+        // Act
+        stockItem.ConfirmReservation(3);
+        
+        // Assert
+        Assert.Equal(1, stockItem.AvailableQuantity);
+        Assert.Equal(1, stockItem.ReservedQuantity);
+        
+        Assert.Empty(stockItem.DomainEvents);
+    }
+
+    [Fact]
+    public void Should_ReleaseReservation_Failed()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        stockItem.AddUnits(5);
+        stockItem.Reserve(2);
+        stockItem.ClearDomainEvents();
+        
+        // Act
+        var releaseReservationAction = () => stockItem.ReleaseReservation(3);
+        
+        // Assert
+        Assert.Throws<InvalidOperationException>(releaseReservationAction);
+    }
+    
+    [Fact]
+    public void Should_ReleaseReservation_Success()
+    {
+        // Arrange
+        var stockItem = new StockItem("sku", "name", 100, Guid.NewGuid());
+        stockItem.AddUnits(5);
+        stockItem.Reserve(3);
+        stockItem.ClearDomainEvents();
+        
+        // Act
+        stockItem.ReleaseReservation(2);
+        
+        // Assert
+        Assert.Equal(4, stockItem.AvailableQuantity);
+        Assert.Equal(1, stockItem.ReservedQuantity);
+        
+        Assert.Contains(stockItem.DomainEvents, domainEvent =>
+        {
+            var isStockLevelChanged = domainEvent is StockLevelChanged;
+
+            if (!isStockLevelChanged)
+            {
+                return false;
+            }
+            
+            var stockLevelChanged = (StockLevelChanged)domainEvent;
+
+            return stockLevelChanged is { Sku: "sku", ChangedQuantity: 2 };
+        });
+    }
+}
